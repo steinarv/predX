0718bf143178f9235f06b5ed48ba6b09d375723e
steinarv <steinar.veka@gmail.com>
2014-05-06 10:35:12 +0200

    Update forecastingModels.cpp

--- src/forecastingModels.cpp
+++ src/forecastingModels.cpp
@@ -377,7 +377,166 @@ SEXP SIMDAYSMOOTHLEVELINPUT(SEXP Y, SEXP L, SEXP DAYS, SEXP S, SEXP OPTNOUT, SEX
 // -------------------------------------------------------------------------------------------------------------------
 
 
+// --------------------------------------------------------------------------------------------------------------------
+// ------------------------------ slight modification on filtering technique ------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 
+SEXP SIMDAYSMOOTH2(SEXP Y, SEXP DAYS, SEXP S, SEXP OPTNOUT, SEXP PARAM, SEXP THOLD, SEXP STARTVAL) {
+	
+	NumericVector nvX(Y); NumericVector nvDAYS(DAYS); int n = nvX.size(); 
+	int f = nvDAYS.size()-n; int s = as<int>(S); int d = 0;
+	int o = as<int>(OPTNOUT);
+	
+	double xhat = 0; // Normalized x when outliers detected
+	double thold = as<double>(THOLD); //Number of standard deviations for treshold (0 < > 4)
+	
+	NumericVector nvPARAM(PARAM); unityFunc(nvPARAM);
+	double alfa = nvPARAM(0); double gamma = nvPARAM(1);
+	
+	NumericVector nvS(s); NumericMatrix nvFIL(n+f, o);
+	NumericVector nvSTARTVAL(STARTVAL);
+	double dVAR = nvSTARTVAL(0); //variance
+	double dL = nvSTARTVAL(1); //level
+	
+	for(int i=0;i<(s);i++)nvS(i)=nvSTARTVAL(i+2);
+	
+
+	for(int i=1;i<(n+f);i++){
+	
+		d = nvDAYS(i);
+		
+		if(i<n){
+			dVAR = 0.06*pow(nvX(i-1)-nvFIL(i-1, 0), 2)+0.94*dVAR;
+			
+			//nvFIL(i, 0)=dL*nvS(d); //Predicted/Filtered value for "today"
+			
+			if(i<=(n-o)){ //Make predictions "o" steps ahead
+				for(int j=0; j<o; j++){
+					d=nvDAYS(i+j);
+					nvFIL(i, j)=dL+nvS(d); //Predicted/Filtered value for "today"
+				}
+				d = nvDAYS(i);
+			}else{
+				nvFIL(i, 0)=dL+nvS(d);
+			}
+			
+			// If x is more than two standard deviation of one step ahead prediction value we set it 
+			// equal to predicted value + thold*sd when updating equations
+			if( nvX(i) < (nvFIL(i, 0)-thold*sqrt(dVAR)) || 
+							nvX(i) > (nvFIL(i, 0)+thold*sqrt(dVAR)) ){
+				
+				nvX(i) < nvFIL(i, 0) ? 
+					xhat = (nvFIL(i, 0)-thold*sqrt(dVAR)) : 
+					xhat = (nvFIL(i, 0)+thold*sqrt(dVAR)) ;
+								
+			
+			}else{
+				xhat=nvX(i);
+			}
+			
+			
+			dL=alfa*(xhat-nvS(d))+(1-alfa)*dL; 	//Level updated with value of today
+			nvS(d)=gamma*(xhat-dL)+(1-gamma)*nvS(d); 	//Trend updated with change in level
+			
+			
+		}else{
+			nvFIL(i, 0) = dL+nvS(d);
+		}
+	}
+	
+	return(wrap(nvFIL));
+}
+// -------------------------------------------------------------------------------------------------------------------
+
+
+
+// ------------------------------ robust simelar day Holt Winters model -----------------------------------------------
+// ----------------- This model calculates OPTNOUT predictions at each step in filtration -----------------------------
+// ------------------------ allowing for more flexible optimization routines ------------------------------------------
+SEXP SIMDAYSMOOTHLEVELINPUT2(SEXP Y, SEXP L, SEXP DAYS, SEXP S, SEXP OPTNOUT, SEXP PARAM, SEXP THOLD, SEXP STARTVAL) {
+	
+	NumericVector nvX(Y); NumericVector nvDAYS(DAYS); int n = nvX.size(); 
+	int f = nvDAYS.size()-n; int s = as<int>(S); int d = 0;
+	int o = as<int>(OPTNOUT);
+	
+	double xhat = 0; // Normalized x when outliers detected
+	double thold = as<double>(THOLD); //Number of standard deviations for treshold (0 < > 4)
+	
+	NumericVector nvPARAM(PARAM); unityFunc(nvPARAM);
+	double alfa = nvPARAM(0); double gamma = nvPARAM(1);
+	
+	NumericVector nvS(s); NumericMatrix nvFIL(n+f, o);
+	
+	NumericVector nvSTARTVAL(STARTVAL);
+	double dVAR = nvSTARTVAL(0); //variance
+	for(int i=0;i<(s);i++)nvS(i)=nvSTARTVAL(i+2);
+	
+	
+	// One smoothed level and one external level input is combined based on historical accuracy
+	double dL = nvSTARTVAL(1); //level
+	double dL1 = dL; // smoothed level
+	NumericVector nvL(L);
+	double dL2 = 0; // input level
+	double r1=0.5; // accuracy of smoothed level
+	double r2=0.5; // accuracy of input level
+	double w1=0.5; // Weight assigned to smoothed level
+	double w2=0.5; // Weight assigned to input level
+	
+	
+	for(int i=1;i<(n+f);i++){
+	
+		d = nvDAYS(i);
+		
+		if(i<n){
+			dVAR = 0.06*pow(nvX(i-1)-nvFIL(i-1, 0), 2)+0.94*dVAR;
+			
+			//nvFIL(i, 0)=dL*nvS(d); //Predicted/Filtered value for "today"
+			
+			if(i<=(n-o)){ //Make predictions "o" steps ahead
+				for(int j=0; j<o; j++){
+					d=nvDAYS(i+j);
+					nvFIL(i, j)=dL+nvS(d); //Predicted/Filtered value for "today"
+				}
+				d = nvDAYS(i);
+			}else{
+				nvFIL(i, 0)=dL+nvS(d);
+			}
+			
+			// If x is more than two standard deviation of one step ahead prediction value we set it 
+			// equal to predicted value + thold*sd when updating equations
+			if( nvX(i) < (nvFIL(i, 0)-thold*sqrt(dVAR)) || 
+							nvX(i) > (nvFIL(i, 0)+thold*sqrt(dVAR)) ){
+				
+				nvX(i) < nvFIL(i, 0) ? 
+					xhat = (nvFIL(i, 0)-thold*sqrt(dVAR)) : 
+					xhat = (nvFIL(i, 0)+thold*sqrt(dVAR)) ;
+								
+			
+			}else{
+				xhat=nvX(i);
+			}
+			
+			//Calculate the optimal combination of the two estimates of the level
+			r1=0.1*pow(xhat/nvS(d)-dL1, 2)+0.9*r1; 	//Accuracy of smoothed level
+			r2=0.1*pow(xhat/nvS(d)-dL2, 2)+0.9*r2;	//Accuracy of input level
+			
+			w1=(1-r1/(r1+r2)); w2=1-w1; //Weights
+			
+			dL1=alfa*(xhat-nvS(d))+(1-alfa)*dL1; 	//Smoothed level updated with value of today
+			dL2=nvL(i);
+			dL=w1*dL1+w2*dL2;
+			
+			nvS(d)=gamma*(xhat-dL)+(1-gamma)*nvS(d); 	//Trend updated with change in level
+			
+		}else{
+			dL=w1*dL1+w2*nvL(i);
+			nvFIL(i, 0) = dL+nvS(d);
+		}
+	}
+	
+	return(wrap(nvFIL));
+}
+// -------------------------------------------------------------------------------------------------------------------
 
 
 
