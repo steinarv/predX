b56a944e6e8c8540848b15c8f58d7807ecfe9535
steinarv <steinar.veka@gmail.com>
2014-05-07 10:08:37 +0200

    Update forecastingModels.cpp

--- src/forecastingModels.cpp
+++ src/forecastingModels.cpp
@@ -523,7 +523,7 @@ SEXP SIMDAYSMOOTHLEVELINPUT2(SEXP Y, SEXP L, SEXP DAYS, SEXP S, SEXP OPTNOUT, SE
 			w1=(1-r1/(r1+r2)); w2=1-w1; //Weights
 			
 			// ------------ delete ------------
-			//std::cout << "w1 and w2: " << w1 << ", " << w2 << std::endl;
+			std::cout << "w1 and w2: " << w1 << ", " << w2 << std::endl;
 			// w1 and w2 seemed to converge to 0.5 and 0.5
 			
 			dL1=alfa*(xhat-nvS(d))+(1-alfa)*dL1; 	//Smoothed level updated with value of today
