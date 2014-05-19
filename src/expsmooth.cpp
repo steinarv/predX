6958b909c7a52b5562539c96f9f6b5b97433df75
steinarv <steinar.veka@gmail.com>
2014-05-19 11:25:45 +0200

    Update expsmooth.cpp

--- src/expsmooth.cpp
+++ src/expsmooth.cpp
@@ -95,7 +95,7 @@ SEXP HW_SIMDAY(SEXP Y, SEXP DAYS, SEXP S, SEXP OPTNOUT, SEXP PARAM, SEXP THOLD, 
 	
 
 	for(int i=1;i<(n+f);i++){
-	std::cout << i << std::endl;
+	std::cout << alfa << "  " << beta << "  " << gamma << std::endl;
 
 		d = nvDAYS(i);
 		
