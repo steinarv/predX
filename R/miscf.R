04ffdb2a9cec92a94ca2b0e5011e1d68f6a8df61
steinarv <steinar.veka@gmail.com>
2014-06-04 09:58:09 +0200

    Update miscf.R

--- R/miscf.R
+++ R/miscf.R
@@ -10,4 +10,17 @@ pow2 <- function(x)x^2
 # ------- Functions for aggregating data across time --------
 aggsum <- function(x)sum(x)
 aggprod <- function(x)prod(1+x)-1
-# -----------------------------------------------------------
+# -----------------------------------------------------------------------------
+
+
+#------------ Inverse unity function ------------------------------------------
+INVunityf <- function(x){
+	x <- sapply(x, FUN=function(z)min(z,0.9999)) #Ensure numeric value in return
+	max(0.00000000000001, log(x/(1-x)))
+	}
+# .............................................................................
+
+# ----------- Score functiona -------------------------------------------------
+fMSE <- function(y, x, trim=0)mean((y-x)^2, trim=trim)
+fABS <- function(y, x, trim=0)mean(abs(y-x), trim=trim)
+# ------------------------------------------------------------------------------
