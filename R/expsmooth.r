f01523aa839254c80cb0e74ac7e63f3a2549cace
steinarv <steinar.veka@gmail.com>
2014-06-04 10:44:13 +0200

    Update expsmooth.r

--- R/expsmooth.r
+++ R/expsmooth.r
@@ -187,7 +187,7 @@ hw_simday <- function(y, days, l=NULL, param=NULL, doOptim=TRUE, opt.nout=7, tre
 	  		param_ <- c(param[1], INVunityf(0), param[2], w1, w2)
 	  	}
 	 
-		opt$par <- 1/(1+exp(-opt$par))
+		
 	}else{
 	 param_ <- param
 	}
@@ -195,6 +195,9 @@ hw_simday <- function(y, days, l=NULL, param=NULL, doOptim=TRUE, opt.nout=7, tre
 	
 	fit <- .Call("HW_SIMDAY", Y=y, DAYS=days, L=l, S=s, OPTNOUT=1, PARAM=param_, THOLD=thold, 	
 			        STARTVAL=startVal, MULT=mult, PACKAGE = "predX" )
+	
+	opt$par <- 1/(1+exp(-param_))
+	names(opt$par) <- c("alpha", "beta", "gamma", "w1", "w2")
 	
 	lOut <- list(startVal=startVal, fitIn=fit[1:n, 1])
 	if(nout>0)lOut <- c(lOut, list(fitOut=fit[(n+1):(n+nout), 1]))
